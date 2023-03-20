0acc889d59e137597ce0f8efaf6435eba5ae8809
advaithbinu11 <71909050+advaithbinu11@users.noreply.github.com>
2023-03-19 19:02:20 -0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,45 @@
+package com.amazonaws.codesamples.document;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+public class Test {
+
+    static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+    static DynamoDB dynamoDB = new DynamoDB(client);
+    static String tableName = "MatchTable";
+
+    public static void main(String[] args) throws Exception {
+
+        PlayerScan();
+    }
+
+    private static void PlayerScan() {
+
+        Table table = dynamoDB.getTable(tableName);
+
+        Map<String, String> expressionAttributeValues = new HashMap<String, String>();
+        expressionAttributeValues.put(":pr", "Advaith");
+
+        ItemCollection<ScanOutcome> items = table.scan("Player1 = :pr", // FilterExpression
+            "Player2, Score", // ProjectionExpression
+            null, // ExpressionAttributeNames - not used in this example
+            expressionAttributeValues);
+
+        System.out.println("Scan of " + tableName + " for items with a price less than 100.");
+        Iterator<Item> iterator = items.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next().toJSONPretty());
+        }
+    }
+
+}
